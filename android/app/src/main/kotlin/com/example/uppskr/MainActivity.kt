81e9632630bf5913f20944b7dcefdd104798abe0
kristjanjonsson <kristjanjonsson96@gmail.com>
2024-05-14 21:28:32 +0000

    UppskriftarApp

---
+++ android/app/src/main/kotlin/com/example/uppskr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uppskr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UppskriftarApp());
+}
+
+class UppskriftarApp extends StatelessWidget {
+  const UppskriftarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Uppskrifir',
+    );
+  }
+}
+
